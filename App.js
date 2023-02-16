35aa15c2611a61eae9931a25ffdbbb3c8ad8bd3f
Ssali Benjamin <ssalibenjamin0402@gmail.com>
2023-02-16 08:40:31 +0300

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions, TextInput} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("BlockSafe");
+
+  const changeHandler = (value) => {
+    setName(value)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.circle}>
+        <Text style={styles.text}>{name}</Text>
+        <TextInput placeholder='Enter a title...' style={styles.input} onChangeText={(val) => changeHandler(val)}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ADD8E6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  circle : {
+    padding: 30,
+    alignItems: 'center',
+    backgroundColor: "#191970",
+    width : Dimensions.get("window").width*1.8,
+    height: Dimensions.get("window").width*1.8,
+    borderRadius: Dimensions.get("window").width,
+    position: "relative",
+    bottom:-Dimensions.get("window").height + Dimensions.get("window").width*1.5
+  },
+
+  text: {
+    color: "white",
+    fontSize: 40
+  }, 
+
+  input : {
+    borderRadius: 5,
+    borderWidth:1,
+    borderColor: "black",
+    backgroundColor: 'white',
+    padding: 8,
+    margin: 10,
+    alignItems: 'center',
+    width: 200,
+    position: "relative",
+    bottom: -Dimensions.get("window").height + Dimensions.get("window").width
+  }
 });
